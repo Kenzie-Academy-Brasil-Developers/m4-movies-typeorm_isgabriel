e6b7ba513ce588ec0b04afd509c9c47222e0a71a
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-04-27 20:07:02 -0300

    feat: CRUD routes made

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -1,4 +1,9 @@
 import { Router } from "express";
 
 const movieRoutes: Router = Router();
+
+movieRoutes.post("");
+movieRoutes.get("");
+movieRoutes.delete("/:id");
+movieRoutes.patch("/:id");
 export default movieRoutes;
